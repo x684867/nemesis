18e13b72f30b33674857b82ab659a50dd7be31a4
x684867 <mail@samcaldwell.net>
2013-11-03 09:38:55 -0600

    Latest store fixes.

--- packages/store/main.js
+++ packages/store/main.js
@@ -1,5 +1,5 @@
 /*
-	Nemesis Data Store Abstraction Layer Package
+	Nemesis Data Store Layer Package
 	/srv/nemesis/packages/store/
 	(c) 2013 Sam Caldwell.  All Rights Reserved.
 	
@@ -19,48 +19,94 @@ module.exports=function(){
 	 */
 	if(typeof(config.server_type)=='undefined') error.raise(error.store.UndefinedServerType);
 	
-	initialize_store();
 	
 	root.store={};
+	var marco=require('./marco/main.js')();
+	var polo=require('./polo/main.js')();
 	
-	root.store.marco=require('./marco/main.js');
-	root.store.polo=require('./polo/main.js');
-	
-	root.store.read:function(objectId){
-		var fs=require('fs');
+	initialize_store(marco,polo);
+	/*
+		read():
+				The read method will attempt to read the object file from disk
+				and if that fails, it will execute store.polo.advertise to 
+				request the object from peers.  It will then retry the read 
+				operation for a pre-determined number of times (pausing for
+				two seconds first.
+	 */
+	root.store.read=function(objectId){
+
 		var buffer=undefined;
-		for(retry_count=0;i<=config.store.readTimeout; retry_count++){
-			fs.readFile(objectPath,function(err,encoded_data){
-				if(err){
-					switch(err.code){
-						case 'ENOENT':store.polo.advertise(objectId);
-						/*
-							Add more error handling
-						 */
-						default:error.raise(error.store.fileReadError,err.toString());break;
+		var retryCount=config.store.read.timeOut;
+		var objectReader=function(objectId,retryCount-1){
+		 	require('fs').readFile(
+				objectPath,
+				function(err,encoded_data){
+					if(err) switch(err.code){
+						case 'ENOENT':
+							error.raise(error.store.objectNotOnHost,objectId);
+							store.polo.advertise(objectId);
+							if(retryCount>0){
+								setTimeout(function(){
+									buffer=objectReader(objectId,retryCount-1);
+								},config.store.read.retryInterval);
+							}
+							break;
+							/*
+								Add more error handling
+							 */
+						default:
+							/*
+								catch all un-handled errors.
+							 */
+							error.raise(error.store.fileReadError,err.toString());
+							buffer=undefined;
+							break;
+					}else{
+						buffer=encoded_data;
+						break;					
 					}
-				}else{
-					buffer=require('base64').decode(encoded_data);
-					break;					
-				}
-			});
+				});
+				/*We have returned data.  Decode it*/
+				return require('base64').decode(buffer);
 		}
-		if(buffer===undefined){
+		if(buffer==undefined){
 			error.raise(error.store.objectNotFound,objectId);
 		}else{
 			return buffer;
 		}
-	}
-	write:function(objectId,objectData){
-	}
+	};
+	/*
+		write():
+				The write method will attempt to write the object file to
+				disk and then send the same to marco for replication.
+	 */
+	root.store.write=function(objectId,objectData){
+		var oStore={
+				"id":objectId,
+				"hash":crypto.createHash('sha512WithRSAEncryption').update(objectData).digest('hex'),
+				"time":(new Date()).getTime(),
+				"data":require('base64').encode(objectData),
+				"ttl":config.store.replication.ttl
+		};
+		require('fs').fileWrite(filename,objectData){
+		});
+		store.marco.push(objectId,oStore);/*Marco will push to a queue and return.*/	
+	};
+}
+function start_write(objectId,oStore){
+	/*
+		start asynchronous write
+	*/
 }
 
-function initialize_store(){
+function initialize_store(marco,polo){
 	/*
 		Initialize the store-as-a-peer service.
 		This means connecting to each registered
 		peer and advertising the current host as
 		online and ready.
 	*/
-	
+	marco.register();
+	polo.startListener();
+	marco.startReplicator();
 }
--- packages/store/marco/main.js
+++ packages/store/marco/main.js
@@ -1,15 +1,73 @@
 /*
-	Nemesis Audit Server package
-	/srv/nemesis/packages/audit/main.js
+	Nemesis Data Store Layer Package
+	/srv/nemesis/packages/store/
 	(c) 2013 Sam Caldwell.  All Rights Reserved.
 	
+	This package provides a peer-to-peer distributed key-value store.
+		
+	USE:
+		root.store
+		
+	DOCUMENTATION:
+	
+		See https://github.com/x684867/nemesis_server/wiki/Framework:-Packages:-Store
+	
 */
-module.exports=init;
+module.exports=function(){
+	
+	var objectQueue=Array(); /*Queue of objects pending replication*/
+	var concurrency=0;			 /*Number of instances of replicate() method currently executing*/
+	
+	register=function(){
+	
+	}
+	startReplicator=function(){
+	
+	}
+	
+	push=function(objectId,oStore){
+	
+	}
+	replicate=function(){
+		if(concurrency >= store.marco.maxConcurrency){
+			/*pause timer*/
+		}
+		rCount++;
+		var curr=objectQueue.pop();
+		if(curr!=undefined){
+			store.peerList.forEach(function(peer,i,a){
+				/*send message*/
+			});
+		}
+		if(concurrency < store.marco.maxConcurrency){
+			/*resume timer*/
+		}
+		rCount--;
+	}
+	/*
+		Start the timer-based queue
+		processor to replicate data
+		objects to remote hosts.
+	*/
+	
+}
 
-function init(){
 
-	console.log("Audit Server not implemented yet.");
-	console.log("see "+package.filename);
-	process.exit(1);
 
+function generateSignature(o){
+	/*
+		Generate a signature that cannot be replayed.
+	 	To do this we hash the id, data and time using
+	 	a sha512 algorithm.  This hash will be encrypted
+	 	using the sender's private key.  Recipients can then
+	 	decrypt the hash with the sender's public key to validate
+	 	the package.
+	 	
+	 	TLS_ECDH_RSA_WITH_AES_256_GCM_SHA384
+	 	
+	 	
+	 */
+	return crypto.createHash('sha512WithRSAEncryption')
+				 .update(o.id+o.hash+o.time+o.data)
+				 .digest('hex');		
 }

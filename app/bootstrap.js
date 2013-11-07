60b969d1398820cac078c8cb5f075a22d34f348a
x684867 <mail@samcaldwell.net>
2013-11-07 07:43:47 -0600

    refactoring bootstrap.

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -24,80 +24,64 @@ require('./JSON-commented.js')();
 require('./JSON-active.js')();
 require('./JSON-config.js')();
 /*Validate the command-line inputs.*/
-var server_type=process.argv[2];
-var launch_mode=process.argv[3];
+(function(){
+	var application=process.argv[2];
+	var launch_mode=process.argv[3];
 
-if(typeof(server_type)!='string') 
-	throw new Error('Invalid server_type passed to boostrap.js');
-if((typeof(launch_mode)!='string') && (['master','worker'].indexof(launch_mode)==-1))
-	throw new Error('Invalid launch_mode passed to bootstrap.js')
-
-console.log( Array(process.stdout.rows).join('\n')+Array(process.stdout.columns).join('=')+
-             '\nStart ['+app.title+':v'+app.version+'] '+
-             server_type+':'+launch_mode+'('+process.pid+') '+ (new Date).toString()+'\n\n');
-/* 
-	Load application configuration data
- */
-if(require('fs').lstatSync(app_conf).isFile()){
-	root.config=JSON.config.loadValidJSON(app_conf,app_conf_pattern);
-	root.config.server_type=server_type
-	if(typeof(config.debug)!='boolean') throw new Error('root.config.debug must be boolean');
-}else
-	throw new Error('app_conf file not found.  Check '+app_conf);
-/* 
-	Load all packages in the manifest.
-*/
-root.packages={};
-root.packages.loader=require(config.packageLoader)(
-														config.packageManifest,
-														server_type,
-														launch_mode
-					);
-/*
-	-----------------------------------------------------------------------------------
-	Define the application
-	-----------------------------------------------------------------------------------
-*/
-/*Initialize then launch the application with the specified service (using arg[2])*/
-root.app.main(server_type,launch_mode);
-
-
-function main(application,launchMode){
-	/*
-		Load the appropriate service configuration file.
+	console.log( Array(process.stdout.rows).join('\n')+
+				 Array(process.stdout.columns).join('=')+
+	             '\nStart ['+app.title+':v'+app.version+'] '+
+	             application+':'+launch_mode+'('+process.pid+') '+ (new Date).toString()+
+	             '\n\n'
+	);
+	/* 
+		Load application configuration data
+	 */
+	if(require('fs').lstatSync(app_conf).isFile()){
+		root.config=JSON.config.loadValidJSON(app_conf,app_conf_pattern);
+		root.config.application=application
+	}else
+		throw new Error('app_conf file not found.  Check '+app_conf);
+	/* 
+		Load all packages in the manifest.
 	*/
-	switch(application){
-		case "audit": 	root.config.service=require(root.config.svc_cfg.audit);break;
-		case "broker":	root.config.service=require(root.config.svc_cfg.broker);break;
-		case "cipher":	root.config.service=require(root.config.svc_cfg.cipher);break;
-		case "key":		root.config.service=require(root.config.svc_cfg.key);break;
-		default: 
-			root.error.throw(root.error.messages.bootstrap.invalidArgument);
-			break;
+	(function(){
+		root.packages={};
+		var pkgLoader=require(config.packageLoader);
+		root.packages.loader=pkgLoader(config.packageManifest,server_type,launch_mode);
+	});
+	/*
+		--------------------------------------------------------------------------------
+		Define the application
+		--------------------------------------------------------------------------------
+	*/
+	switch(launch_mode){
+		"master":
+				/*
+					Assumptions:
+						1. 	PeerNet started at package load.
+						2. 	storeServer started at package load.
+						3. 	statsCollector started at package load.
+						4. 	watchdogProcess started at package load.
+						5. 	Each of the above packages tainted process with timer-based
+							events during their initialization.
+				 	What needs to be done:
+				 		1. How do we handle IPC messages for the above?
+				 */
+				 
+		"worker":
+				/*
+					Assumptions:
+						1. peerNetClient is exposed for use.
+						2. storeClient is exposed for use.
+						3. statsAgent is exposed for use.
+						4. watchdogListener is exposed for use.
+						5. workerCore is exposed for use.
+						6. webCore is exposed for use.
+						7. The appropriate application will launch on its package load.
+				*/
+		default:
+				throw new Error('Application failed to launch. launch_mode is invalid');
 	}
-	/*
-		Show the application banner	
-	*/
-	root.app.log.screenBanner(root.message.app.starting);
-	/*
-		Initialize the process manager
-	*/
-	root.process=require(root.config.packages.app.process);
-	root.process.init();
-	/*
-		Launch the application
-	*/	
-	if(root.app.start()){
-		if(root.app.monitor.heartbeat.start()){
-			if(root.app.monitor.statistics.start()){
-				root.app.log.error(root.error.app.main.success);
-			}else{
-				root.app.log.error(root.error.app.main.monitorStatisticsFailed);
-			}
-		}else{
-			root.app.log.error(root.error.app.main.monitorHeartbeatFailed);
-		}
-	}else{
-		root.app.log.error(root.error.app.main.servicesFailed);
-	}
-}
+	root.app.main(application,launch_mode);
+});
